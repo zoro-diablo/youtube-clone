f35a724ead52d7efe97cbdc8bb249d2822e3aaa3
Vyshnav.K <vyshnavk117@gmail.com>
2024-02-02 11:41:06 +0530

    Refactor Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,8 @@
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
-import { useState } from 'react';
 
 const Navbar = () => {
-  const [active, setActive] = useState(false);
-
   return (
     <Stack
       className='navbar'
@@ -19,9 +16,11 @@ const Navbar = () => {
         justifyContent: 'space-between',
       }}
     >
-      <div style={{display:'flex',}}>
-       
-        <Link to='/' style={{ display: 'flex', alignItems: 'center' , marginLeft:'15px' }}>
+      <div style={{ display: 'flex' }}>
+        <Link
+          to='/'
+          style={{ display: 'flex', alignItems: 'center', marginLeft: '15px' }}
+        >
           <img src='/logo-2.png' alt='logo' width={150} />
         </Link>
       </div>
